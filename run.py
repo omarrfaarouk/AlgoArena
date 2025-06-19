56036d9395544dd6fff1fd05ce0679619ff66339
Omar Farouk <you@example.com>
2025-06-19 00:12:53 -0500

    Use dynamic port from environment for Render

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(port=5005, host='0.0.0.0', debug=True)  # 5005 is rarely used by system
+    port = int(os.environ.get('PORT', 5005))
+    app.run(host='0.0.0.0', port=port, debug=True)
